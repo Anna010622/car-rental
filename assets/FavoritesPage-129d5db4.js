7140f3006a2ec3efa262a3cc89940e8db9e75217
Anna Trifonova <3809322999@ukr.net>
2023-09-13 01:49:57 +0300

    Updates

--- assets/FavoritesPage-129d5db4.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-78d682c5.js";const t=()=>e.jsx("h1",{children:"FavoritePage"});export{t as default};
---
+++ assets/MainPage-91b4fb37.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-ed41109c.js";const e=()=>s.jsx("section",{className:"section"});export{e as default};
