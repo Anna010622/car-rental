f81cd0e8dfa062c92314d32642b03d838423ef52
Anna Trifonova <3809322999@ukr.net>
2023-09-13 01:55:37 +0300

    fix deploying

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-	plugins: [react()],
 	base: '/car-rental',
+	plugins: [react()],
 	css: {
 		preprocessorOptions: {
 			scss: {
