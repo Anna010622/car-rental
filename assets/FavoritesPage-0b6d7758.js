a08038cfd0278766f19c653d4daa8c666cb1c1ec
Anna Trifonova <3809322999@ukr.net>
2023-09-15 16:50:34 +0300

    Updates

--- assets/FavoritesPage-0b6d7758.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-8bebc217.js";const t=()=>e.jsx("h1",{children:"FavoritePage"});export{t as default};
---
+++ assets/HomePage-8d87f67f.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-5c042e71.js";const r=()=>e.jsx("h1",{children:"Home"});export{r as default};
